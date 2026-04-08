801cd0b31996114ab93cb322f95ec1f93b9eae9f
Jose Antonio Alcala Hinojosa <jah0006@alu.medac.es>
2026-04-08 20:18:17 +0200

    creacion tablero básico con bucles y matrices

--- Monopoly/src/App.java
+++ Monopoly/src/App.java
@@ -1,5 +1,29 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String[][] tablero = new String[10][10];
+
+
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero.length; j++) {
+                if (i==0) {
+                    tablero[i][j]=" O ";
+                }else if (i==9) {
+                    tablero[i][j]=" O ";
+                }else if (j==9) {
+                    tablero[i][j]=" O ";
+                }else if (j==0) {
+                    tablero[i][j]=" O ";
+                }else{
+                    tablero[i][j]="   ";
+                }
+            }
+        }
+
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero.length; j++) {
+                System.out.print(tablero[i][j]);
+            }System.out.println();
+        }
     }
 }
